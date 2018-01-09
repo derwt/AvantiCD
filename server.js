62f364ed4977b378e9c09f2ae8ae832e640cd7bf
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-09 15:35:32 -0800

    Added nodemon package to refresh server whenever a file the server uses is saved. Start server with `npm run dev`

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
+const PORT = 27017;
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const URL = 'mongodb://localhost:27017'
 
-const url = "mongodb://localhost:27017/avantiCD";
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
+
+app.get('/', (req, res, next) => {
+  res.send(__dirname + '/index.html');
+})
+
+// MongoClient.connect(URL, (err, client) => {
+//   if (err) return;
+//
+//   const db = client.db('avantiCD');
+//   const collection = db.collection('Customers');
+//
+//   collection.insert({CID: 9999, 'Phone Number': 650999999}, (err, result) => {
+//     collection.find({CID: 9999}).toArray((err, docs) => {
+//       console.log(docs[0])
+//       client.close()
+//     })
+//   })
+// })
