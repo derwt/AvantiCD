a932198f9789c779d5412003d477ba7e256227bc
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-09 21:02:20 -0800

    Uploading CustomerDataConverter .json

--- Python/CustomerDataConverter.py
+++ Python/CustomerDataConverter.py
@@ -1,54 +1,58 @@
-import csv
+import json
 import time
-import AddressCorrecter as AC
 
 class CustomerDataConverter:
         
     def __init__(self):
         self.section_one_ending = 3165
         self.customer_data = {}
+        self.customers = []
         self.customers_failed = 0
-        self.address_corrector = AC.AddressCorrecter()
+        # self.address_corrector = AC.AddressCorrecter()
 
-    def _convert_txt_to_csv(self, txt_filename, csv_filename):
-        with open(txt_filename, 'r') as infile, open(csv_filename, 'w', newline='') as outfile:
-            csvwriter = csv.writer(outfile)
+    def _convert_txt_to_csv(self, txt_filename, json_filename):
+        with open(txt_filename, 'r') as infile, open(json_filename, 'w', newline='') as outfile:
             for i, line in enumerate(infile):
                 split_line = line.split(':')
                 if i < self.section_one_ending:
-                    CID = int(split_line[1].strip())
+                    cID = int(split_line[1].strip())
                     phone_number = int(split_line[0])
-                    self._add_customer(CID, phone_number)
+                    if not self._customer_exists(cID):
+                        self._add_customer(cID, phone_number)
+                    else:
+                        self.customer_data[cID]._add_phone_number(phone_number)
 
                 elif i == self.section_one_ending:
                     continue
                 else:
-                    CID = int(split_line[0])
+                    cID = int(split_line[0])
                     city = split_line[1]
-                    address = self.address_corrector._lookup_one(split_line[2], self._get_city_from_abbrv(city))
-                    if not address:
-                        address = split_line[2]
-                    print(address)
+                    # address = self.address_corrector._lookup_one(split_line[2], self._get_city_from_abbrv(city))
+                    # if not address:
+                    address = split_line[2]
                     cross_street = split_line[3]
                     note = split_line[4]
-                    time = split_line[5]
+                    time_last_ordered = split_line[5].strip()
                     try:
-                        self._complete_customer(CID, city, address, cross_street, note, time)
-                        csvwriter.writerow(self.customer_data[CID]._get_customer_data(CID), )
+                        self._complete_customer(cID, city, address, cross_street, note, time_last_ordered)
+                        self.customers.append(self.customer_data[cID]._get_json_entry(cID))
+                        # csvwriter.writerow(self.customer_data[cID]._get_customer_data(cID), )
                     except Exception as ex:
                         self.customers_failed += 1
             print("Customers Failed: " + str(self.customers_failed))
-            self.customer_data[1297]._print_customer_data()
+            self.customer_data[1847]._print_customer_data()
+            print(self.customer_data[1337].phone_numbers)
+            json.dump(self.customers, outfile)
 
-    def _add_customer(self, CID, phone_number):
-        self.customer_data[CID] = Customer(phone_number)  
+    def _add_customer(self, cID, phone_number):
+        self.customer_data[cID] = Customer(phone_number)  
 
-    def _complete_customer(self, CID, city, address, cross_street, note, time):
-        self.customer_data[CID].city = city
-        self.customer_data[CID].address = address
-        self.customer_data[CID].cross_street = cross_street
-        self.customer_data[CID].note = note
-        self.customer_data[CID].time_last_ordered = time
+    def _complete_customer(self, cID, city, address, cross_street, note, time):
+        self.customer_data[cID].city = city
+        self.customer_data[cID].address = address
+        self.customer_data[cID].cross_street = cross_street
+        self.customer_data[cID].note = note
+        self.customer_data[cID].time_last_ordered = time
 
     def _get_city_from_abbrv(self, abbreviation):
         return {
@@ -62,10 +66,15 @@ class CustomerDataConverter:
             "":""
         }[abbreviation]
 
-class Customer:
+    def _customer_exists(self, cID):
+        return cID in self.customer_data
 
-    def __init__(self, number=0, city="", address="", cross_street="", note="", time_last_ordered=0):
-        self.phone_number = number
+class Customer:
+    """ Contains all data 
+    """
+
+    def __init__(self, numbers=0, city="", address="", cross_street="", note="", time_last_ordered=0):
+        self.phone_numbers = [numbers]
         self.city = city
         self.address = address
         self.cross_street = cross_street
@@ -78,13 +87,27 @@ class Customer:
         self.address = address
         self.cross_street = cross_street
         self.note = note
-        self.time_last_ordered = time_last_ordered
+        self.time_last_ordered = int(time_last_ordered.strip())
 
-    def _get_customer_data(self, CID):
-        return [CID, self.phone_number, self.city, self.address, self.cross_street, self.note, self.time_last_ordered]
+    def _get_customer_data(self, cID):
+        return [cID, self.phone_numbers, self.city, self.address, self.cross_street, self.note, self.time_last_ordered]
+
+    def _add_phone_number(self, number):
+        self.phone_numbers.append(number)
+
+    def _get_json_entry(self, cID):
+        return {
+        'CID':cID,
+        'Phone Numbers':self.phone_numbers,
+        'City':self.city,
+        'Address':self.address,
+        'Cross-Street':self.cross_street,
+        'Note':self.note,
+        'Last Time Ordered':self.time_last_ordered
+        }
 
     def _print_customer_data(self):
-        print("Phone #:    " + str(self.phone_number))
+        print("Phone #:    " + str(self.phone_numbers))
         print("City:       " + self.city)
         print("Address:    " + self.address)
         print("Cross:      " + self.cross_street)
@@ -94,4 +117,7 @@ class Customer:
 
 if __name__ == "__main__":
     cdc = CustomerDataConverter()
-    cdc._convert_txt_to_csv("numbers.txt", "customers.csv")
+    cdc._convert_txt_to_csv("numbers.txt", "customers.json")
+    # pythonDictionary = {'name':'Bob', 'age':44, 'isEmployed':True}
+    # dictionaryToJson = json.dumps(cdc.customers)
+    # print(dictionaryToJson)
