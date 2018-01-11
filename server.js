107d6b4b4986e54b770501617fa79265033f0bf6
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-10 23:17:30 -0800

    Creating Mongoose schema 'customerSchema' and refactoring database to work with Mongoose

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-const express = require('express')
-const app = express()
-const PORT = 27017
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const URL = 'mongodb://localhost:27017'
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 27017;
+const db = require('./db');
 
-app.use(bodyParser.urlencoded({extended: true}))
+// Require CID routes
+const customersRouter = require('./customers.js');
+app.use('/customers', customersRouter);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/quotes', (req, res, next) => {
-  console.log(req.body)
-})
-
-MongoClient.connect(URL, (err, client) => {
-  if (err) return console.log(err);
-
-  var db = client.db('avantiCD');
-  const collection = db.collection('Customers');
-
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   })
 
-  // app.post('/quotes', (req, res, next) => {
-  //   collection.insert(req.body, (err, res, next) => {
-  //     if (err) return console.log(err)
-  //
-  //     console.log('saved to database')
-  //     res.redirect('/')
-  //   })
-  // })
-  app.post('/quotes', (req, res, next) => {
-    collection.insert({CID: 9998, 'Phone Number': 650999999}, (err, result) => {
-      collection.find({CID: 9998}).toArray((err, docs) => {
-        console.log(docs[0]);
-        client.close();
-      });
-    });
-  })
-
-})
+  app.get('/', (req, res, next) => {
+      res.send('Hooray!');
+  });
