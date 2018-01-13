0d6c46b77c965e1595327f47ef2c726ad13810a5
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-13 15:42:13 -0800

    Adding get and put skeletons to routes

--- app/models/Customer.js
+++ app/models/Customer.js
@@ -15,7 +15,7 @@ var customerSchema = new Schema({
 }, {collection: 'Customers'});
 
 // Define Customer methods
-customerSchema.methods.getPhoneNumbers =     function() { for (var i = 0; i < this.phone.length; i++) { console.log(this.phone[i]); } };
+customerSchema.methods.getPhoneNumbers = function() { for (var i = 0; i < this.phone.length; i++) { console.log(this.phone[i]); } }
 
 // Customer = mongoose.model('Customer', customerSchema);
 // var c = new Customer({ cid: 9997, phone: [10, 20], city: "BM", address: "2700 Yo Dr.", cross: "Cross St", note: "Note to self!", ordered: 999999});
