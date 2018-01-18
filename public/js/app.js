f800647285d3a9a6a15a0a3d9a4a8f82f46617b2
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-18 10:58:20 -0800

    Added angular telephone filter and applied it in ID Card

--- public/js/app.js
+++ public/js/app.js
@@ -18,6 +18,53 @@ app.directive('titleContent', function() {
       content: '='
     },
     template: '<div class="text-center"> <h4>{{ title }}</h4> <p class="black-text">{{ content }}</p> </div>',
-    link: (scope,elem,attrs) => {}
+    link: (scope, elem, attrs) => {}
+  };
+});
+
+app.filter('telephone', function() {
+  return function(telephone) {
+    if (!telephone) {
+      return '';
+    }
+
+    var value = telephone.toString().trim().replace(/^\+/, '');
+
+    if (value.match(/[^0-9]/)) {
+      return telephone;
+    }
+
+    var country, city, number;
+
+    switch (value.length) {
+      case 10: // +1PPP####### -> C (PPP) ###-####
+        country = 1;
+        city = value.slice(0, 3);
+        number = value.slice(3);
+        break;
+
+      case 11: // +CPPP####### -> CCC (PP) ###-####
+        country = value[0];
+        city = value.slice(1, 4);
+        number = value.slice(4);
+        break;
+
+      case 12: // +CCCPP####### -> CCC (PP) ###-####
+        country = value.slice(0, 3);
+        city = value.slice(3, 5);
+        number = value.slice(5);
+        break;
+
+      default:
+        return telephone;
+    }
+
+    if (country == 1) {
+      country = "";
+    }
+
+    number = number.slice(0, 3) + '-' + number.slice(3);
+
+    return (country + " (" + city + ") " + number).trim();
   };
 });
