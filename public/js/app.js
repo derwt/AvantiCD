aa56a720cf82030f6b57c2d0e6b499b8611ccc4c
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-17 23:14:56 -0800

    Created info-content directive and worked on ID Cards

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,15 @@ app.directive('idCard', function() {
     templateUrl: 'js/directives/id-card.html'
   };
 });
+
+app.directive('titleContent', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      title: '@title',
+      content: '='
+    },
+    template: '<div class="text-center"> <h4>{{ title }}</h4> <p class="black-text">{{ content }}</p> </div>',
+    link: (scope,elem,attrs) => {}
+  };
+});
