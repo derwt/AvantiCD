12a6c8c0bf38a1f188f1026fe650421ef80ddb3d
Drew Thompson <d_thompson6@u.pacific.edu>
2018-01-21 13:55:55 -0800

    Lookups from search bar populate cards, closes #17, closes #19

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -3,73 +3,76 @@ angular.module('MainController', []).controller('MainController', ['$scope', '$h
   $scope.tagline = 'Let\'s get some pizza ready!';
   $scope.customers =
   [
-    {
-      cid: 2349,
-      phone: [6506782755, 6504008921, 4539990000],
-      city: "BL",
-      address: "612  Southview Court",
-      cross: "South Road",
-      note: "Michael the pizza face. Is cool!",
-      ordered: 1511741627
-    },
-    {
-      cid: 3065,
-      phone: [6504008921, 6505954446],
-      city: "SC",
-      address: "2040 Ralston Avenue",
-      cross: "Alameda",
-      note: "Michael the pizza face. Is Not Cool!",
-      ordered: 1464389861
-    },
-    {
-      cid: 3065,
-      phone: [6504008921, 6505954446],
-      city: "SM",
-      address: "2040 Ralston Avenue",
-      cross: "Alameda",
-      note: "Michael the pizza face. Is Not Cool!",
-      ordered: 1464389861
-    },
-    {
-      cid: 3065,
-      phone: [6504008921, 6505954446],
-      city: "RWS",
-      address: "2040 Ralston Avenue",
-      cross: "Alameda",
-      note: "Michael the pizza face. Is Not Cool!",
-      ordered: 1464389861
-    },
-    {
-      cid: 3065,
-      phone: [6504008921, 6505954446],
-      city: "RWC",
-      address: "2040 Ralston Avenue",
-      cross: "Alameda",
-      note: "Michael the pizza face. Is Not Cool!",
-      ordered: 1464389861
-    },
-    {
-      cid: 3065,
-      phone: [6504008921, 6505954446],
-      city: "FC",
-      address: "2040 Ralston Avenue",
-      cross: "Alameda",
-      note: "Michael the pizza face. Is Not Cool!",
-      ordered: 1464389861
-    },
-    {
-      cid: 3065,
-      phone: [6504008921, 6505954446],
-      city: "HB",
-      address: "2040 Ralston Avenue",
-      cross: "Alameda",
-      note: "Michael the pizza face. Is Not Cool!",
-      ordered: 1464389861
-    }
+    // {
+    //   cid: 2349,
+    //   phone: [6506782755, 6504008921, 4539990000],
+    //   city: "BL",
+    //   address: "612  Southview Court",
+    //   cross: "South Road",
+    //   note: "Michael the pizza face. Is cool!",
+    //   ordered: 1511741627
+    // },
+    // {
+    //   cid: 3065,
+    //   phone: [6504008921, 6505954446],
+    //   city: "SC",
+    //   address: "2040 Ralston Avenue",
+    //   cross: "Alameda",
+    //   note: "Michael the pizza face. Is Not Cool!",
+    //   ordered: 1464389861
+    // },
+    // {
+    //   cid: 3065,
+    //   phone: [6504008921, 6505954446],
+    //   city: "SM",
+    //   address: "2040 Ralston Avenue",
+    //   cross: "Alameda",
+    //   note: "Michael the pizza face. Is Not Cool!",
+    //   ordered: 1464389861
+    // },
+    // {
+    //   cid: 3065,
+    //   phone: [6504008921, 6505954446],
+    //   city: "RWS",
+    //   address: "2040 Ralston Avenue",
+    //   cross: "Alameda",
+    //   note: "Michael the pizza face. Is Not Cool!",
+    //   ordered: 1464389861
+    // },
+    // {
+    //   cid: 3065,
+    //   phone: [6504008921, 6505954446],
+    //   city: "RWC",
+    //   address: "2040 Ralston Avenue",
+    //   cross: "Alameda",
+    //   note: "Michael the pizza face. Is Not Cool!",
+    //   ordered: 1464389861
+    // },
+    // {
+    //   cid: 3065,
+    //   phone: [6504008921, 6505954446],
+    //   city: "FC",
+    //   address: "2040 Ralston Avenue",
+    //   cross: "Alameda",
+    //   note: "Michael the pizza face. Is Not Cool!",
+    //   ordered: 1464389861
+    // },
+    // {
+    //   cid: 3065,
+    //   phone: [6504008921, 6505954446],
+    //   city: "HB",
+    //   address: "2040 Ralston Avenue",
+    //   cross: "Alameda",
+    //   note: "Michael the pizza face. Is Not Cool!",
+    //   ordered: 1464389861
+    // }
   ];
 
   $scope.select = (idCard) => {
     $scope.selected = idCard;
+
+    // TODO: Update call to UI
+
   }
 
   $scope.isSelected = (idCard) => {
@@ -82,12 +85,13 @@ angular.module('MainController', []).controller('MainController', ['$scope', '$h
   $(phoneInput).on('input', (e) => {
     if (!phoneInputReady(phoneInput.val())) return;
 
-      console.log(phoneInput.val());
+      $http.get('http://localhost:27017/customers/' + phoneInput.val())
+      .then((response) => {
 
-      $http.get("http://localhost:27017/customers/6504008921").then(function(response) {
-        $scope.myWelcome = response.data;
-        console.log($scope.myWelcome);
-    });
+        $scope.customers = response.data.slice();
+
+      });
+
   });
 
 //   $http({
